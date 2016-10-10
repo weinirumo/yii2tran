524c8df8e48434f9f7561d37de98e2bec7680fcb
weini <weinirumo@126.com>
2016-10-10 23:22:11 +0800

    todo

--- vendor/yiisoft/yii2/BaseYii.php
+++ vendor/yiisoft/yii2/BaseYii.php
@@ -82,18 +82,22 @@ class BaseYii
     public static $classMap = [];
     /**
      * @var \yii\console\Application|\yii\web\Application the application instance
+     * 属性 应用的实例
      */
     public static $app;
     /**
      * @var array registered path aliases
+     * 属性 数组 注册的路径别名
      * @see getAlias()
      * @see setAlias()
      */
     public static $aliases = ['@yii' => __DIR__];
     /**
      * @var Container the dependency injection (DI) container used by [[createObject()]].
+     * createOject方法使用的依赖注入容器
      * You may use [[Container::set()]] to set up the needed dependencies of classes and
      * their initial property values.
+     * 你可以使用Container::set方法设置类需要的依赖关系和他们初始的属性值
      * @see createObject()
      * @see Container
      */
@@ -102,7 +106,9 @@ class BaseYii
 
     /**
      * Returns a string representing the current version of the Yii framework.
+     * 返回Yii框架的版本号。
      * @return string the version of Yii framework
+     * 返回值 字符串 Yii框架的版本号
      */
     public static function getVersion()
     {
@@ -111,38 +117,58 @@ class BaseYii
 
     /**
      * Translates a path alias into an actual path.
+     * 把路径别名转化成一个真实的路径
      *
      * The translation is done according to the following procedure:
+     * 转化的步骤如下：
      *
      * 1. If the given alias does not start with '@', it is returned back without change;
+     * 1. 如果提供的别名没有以@开始，不做任何处理直接返回；
      * 2. Otherwise, look for the longest registered alias that matches the beginning part
      *    of the given alias. If it exists, replace the matching part of the given alias with
      *    the corresponding registered path.
+     * 2. 否则，查找匹配给定别名开头的最长注册的别名。如果存在，根据相应的注册路径替换
+     * 给定的别名的匹配部分
+     * 
      * 3. Throw an exception or return false, depending on the `$throwException` parameter.
+     * 3 抛出异常或者返回false，取决于$throwException的参数
      *
      * For example, by default '@yii' is registered as the alias to the Yii framework directory,
      * say '/path/to/yii'. The alias '@yii/web' would then be translated into '/path/to/yii/web'.
+     * 例如，默认@yii被注册为Yii框架目录的别名，就是/path/to/yii.别名@yii/web就会被转化为
+     * /path/to/yii/web
      *
      * If you have registered two aliases '@foo' and '@foo/bar'. Then translating '@foo/bar/config'
      * would replace the part '@foo/bar' (instead of '@foo') with the corresponding registered path.
      * This is because the longest alias takes precedence.
+     * 若果你注册了两个别名@foo和@foo/bar，转化@foo/bar/config的时候会使用@foo/bar,而不是@foo,
+     * 因为最长的路径别名优先。
      *
      * However, if the alias to be translated is '@foo/barbar/config', then '@foo' will be replaced
      * instead of '@foo/bar', because '/' serves as the boundary character.
+     * 还有，如果将要被转化的路径是@foo/bar/config,那么@foo将会被替换，而不是@foo/bar，
+     * 因为/代表着最上一级
      *
      * Note, this method does not check if the returned path exists or not.
+     * 注意，该方法不会检测返回的路径是否存在。
      *
      * @param string $alias the alias to be translated.
+     * 参数 字符串 将要被转换的别名
      * @param boolean $throwException whether to throw an exception if the given alias is invalid.
+     * 参数 boolean 如果给定的路径别名不合法，是否抛出异常
      * If this is false and an invalid alias is given, false will be returned by this method.
+     * 如果该值为false，并且提供了不合法的别名，该方法会返回false
      * @return string|boolean the path corresponding to the alias, false if the root alias is not previously registered.
+     * 返回值 字符串或boolean 根据别名产生的路径，如果根别名当前没有注册，就会返回false
      * @throws InvalidParamException if the alias is invalid while $throwException is true.
+     * 当$throwException为true，并且别名不合法
      * @see setAlias()
      */
     public static function getAlias($alias, $throwException = true)
     {
         if (strncmp($alias, '@', 1)) {
             // not an alias
+            //不是一个别名
             return $alias;
         }
 
@@ -170,10 +196,15 @@ class BaseYii
 
     /**
      * Returns the root alias part of a given alias.
+     * 返回给定别名的根别名部分
      * A root alias is an alias that has been registered via [[setAlias()]] previously.
+     * 根别名是之前使用setAlias注册过的别名。
      * If a given alias matches multiple root aliases, the longest one will be returned.
+     * 如果给定的别名匹配了多个根别名，返回最长的那一个
      * @param string $alias the alias
+     * 参数 字符串 别名
      * @return string|boolean the root alias, or false if no root alias is found
+     * 返回值 字符串或boolean 根别名，当根别名没有找到时返回false
      */
     public static function getRootAlias($alias)
     {
@@ -197,30 +228,43 @@ class BaseYii
 
     /**
      * Registers a path alias.
+     * 注册路径别名
      *
      * A path alias is a short name representing a long path (a file path, a URL, etc.)
      * For example, we use '@yii' as the alias of the path to the Yii framework directory.
+     * 路径别名是使用一个短路径表示一个长路径（文件路径，url等）
+     * 例如，我们使用@yii作为yii框架目录的别名
      *
      * A path alias must start with the character '@' so that it can be easily differentiated
      * from non-alias paths.
+     * 路径别名一定要以字符@开始，跟非路径别名以示区分。
      *
      * Note that this method does not check if the given path exists or not. All it does is
      * to associate the alias with the path.
+     * 注意该方法不会检测给定的路径是否存在。它所有的功能就是把路径和别名联系在一起
      *
      * Any trailing '/' and '\' characters in the given path will be trimmed.
+     * 给定路径后面的/和\将会被去掉。
      *
      * @param string $alias the alias name (e.g. "@yii"). It must start with a '@' character.
+     * 参数 字符串 别名（例如@yii），必须以@开头
      * It may contain the forward slash '/' which serves as boundary character when performing
      * alias translation by [[getAlias()]].
+     *它可以包含/,getAlias方法转化路径的时候/可以当做分隔符
      * @param string $path the path corresponding to the alias. If this is null, the alias will
      * be removed. Trailing '/' and '\' characters will be trimmed. This can be
+     * 参数 字符串 跟别名相关的路径。如果是null，将会把别名删除。会去掉最后的/和\，可以是：
      *
      * - a directory or a file path (e.g. `/tmp`, `/tmp/main.txt`)
+     * - 目录或者文件路径，例如/tmp,/tmp/main.txt
      * - a URL (e.g. `http://www.yiiframework.com`)
+     * - url (例如http://www.yiiframework.com )
      * - a path alias (e.g. `@yii/base`). In this case, the path alias will be converted into the
      *   actual path first by calling [[getAlias()]].
+     * - 路径别名。这种情况下，路径别名首先将会被getAlias方法转化为实际路径。
      *
      * @throws InvalidParamException if $path is an invalid alias.
+     * 抛出不合法参数异常， 如果别名不合法
      * @see getAlias()
      */
     public static function setAlias($alias, $path)
@@ -262,25 +306,37 @@ class BaseYii
 
     /**
      * Class autoload loader.
+     * 自动加载类的方法
      * This method is invoked automatically when PHP sees an unknown class.
+     * 当PHP遇到未知的类的时候，自动调用此方法
      * The method will attempt to include the class file according to the following procedure:
+     * 该方法会根据如下步骤尝试包含类文件：
      *
      * 1. Search in [[classMap]];
+     * 1. 在类列表里搜索
      * 2. If the class is namespaced (e.g. `yii\base\Component`), it will attempt
      *    to include the file associated with the corresponding path alias
      *    (e.g. `@yii/base/Component.php`);
+     * 2. 如果类包含命名空间（例如 yii\base\Component），它将会尝试包含跟路径别名相关的文件
+     * 例如 @yii/base/Component.php
      *
      * This autoloader allows loading classes that follow the [PSR-4 standard](http://www.php-fig.org/psr/psr-4/)
      * and have its top-level namespace or sub-namespaces defined as path aliases.
+     * 该自动加载可以加载符合[PSR-4 标准的类]，并且用于路径别名定义的最高级别的命名空间或者子命名
      *
      * Example: When aliases `@yii` and `@yii/bootstrap` are defined, classes in the `yii\bootstrap` namespace
      * will be loaded using the `@yii/bootstrap` alias which points to the directory where bootstrap extension
      * files are installed and all classes from other `yii` namespaces will be loaded from the yii framework directory.
+     * 例如，当别名@yii和@yii/bootstrap被定义过，在yii\bootstrap命名空间下的类将会被使用指向bootstrap扩展安装目录@yii/bootstrap别名
+     * 并且yii命名空间下的其他类将会从yii框架目录加载
      *
      * Also the [guide section on autoloading](guide:concept-autoloading).
+     *还有[自动加载指导部分](guide:concept-autoloading)
      *
      * @param string $className the fully qualified class name without a leading backslash "\"
+     * 参数 字符串 没有\斜线开头的完整的合格的类名
      * @throws UnknownClassException if the class does not exist in the class file
+     * 抛出未知类异常。 如果类在类文件中不存在
      */
     public static function autoload($className)
     {
@@ -368,6 +424,7 @@ class BaseYii
 
     /**
      * @return Logger message logger
+     * 返回值 信息日志
      */
     public static function getLogger()
     {
