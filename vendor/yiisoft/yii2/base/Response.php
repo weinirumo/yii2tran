c92481d84074642b20355e5a430f0b05a59ec38e
weini <weinirumo@126.com>
2017-01-23 13:21:11 +0800

    Response翻译完成！

--- vendor/yiisoft/yii2/base/Response.php
+++ vendor/yiisoft/yii2/base/Response.php
@@ -9,6 +9,7 @@ namespace yii\base;
 
 /**
  * Response represents the response of an [[Application]] to a [[Request]].
+ * Response表示[[Application]]到[[Request]]的响应。
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
@@ -17,13 +18,17 @@ class Response extends Component
 {
     /**
      * @var integer the exit status. Exit statuses should be in the range 0 to 254.
+     * 变量 执行 退出状态。退出状态应该在0到254之间。
+     *
      * The status 0 means the program terminates successfully.
+     * 状态0表示该程序成功结束。
      */
     public $exitStatus = 0;
 
 
     /**
      * Sends the response to client.
+     * 把响应发送到客户端。
      */
     public function send()
     {
@@ -31,10 +36,12 @@ class Response extends Component
 
     /**
      * Removes all existing output buffers.
+     * 移除所有存在的输出缓冲区。
      */
     public function clearOutputBuffers()
     {
         // the following manual level counting is to deal with zlib.output_compression set to On
+        // 下面的手动水平计数取决于zlib.output_compression的设置。
         for ($level = ob_get_level(); $level > 0; --$level) {
             if (!@ob_end_clean()) {
                 ob_clean();
